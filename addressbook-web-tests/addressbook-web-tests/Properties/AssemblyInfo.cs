678b31eded4cdbff7b1760e9a548da588c2e6ed9
aagera34 <aagera34@mts.ru>
2019-09-30 13:23:10 +0300

    Исправление проекта

--- addressbook-web-tests/addressbook-web-tests/Properties/AssemblyInfo.cs
+++ addressbook-web-tests/addressbook-web-tests/Properties/AssemblyInfo.cs
@@ -5,13 +5,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("addressbook-web-tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("addressbook-web-tests")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("000dcee0-40ca-4778-baff-7be357873ca7")]
+[assembly: Guid("a08f4f76-1b50-4a43-b69c-94e591d075b5")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
