1086c3df725ed04fdf87bd950f2b260a9b8dbe0e
aagera34 <aagera34@mts.ru>
2019-10-28 16:12:22 +0300

    Задание 9.

--- addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTests.cs
@@ -79,7 +79,16 @@ namespace WebAddressbookTests
             contact.Phone2 = "";
             contact.Notes = "";
 
+            List<ContactData> oldContacts = app.Contacts.GetContactList();
+
             app.Contacts.Create(contact);
+
+            List<ContactData> newContacts = app.Contacts.GetContactList();
+            oldContacts.Add(contact);
+            oldContacts.Sort();
+            newContacts.Sort();
+
+            Assert.AreEqual(oldContacts, newContacts);
 
         }
     }
